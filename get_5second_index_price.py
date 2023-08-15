614dc447812e3b60a2149af6b5abd66e03cb3279
Andy <sdlin1988@yahoo.com>
2023-08-15 14:27:06 +0800

    brain clear

--- get_5second_index_price.py
+++ get_5second_index_price.py
@@ -12,38 +12,35 @@ import random
 y = '2023'
 
 opt_path = rf'C:\Users\ANDY\Documents\股市資料\資料\期交所_每秒成交資料_2018-now\選擇權\歷史每秒資料_rpt\{y}'
-taiex5_path = f'./證交所_5秒大盤指數資料_2023-now/{y}'
+taiex5_path = f'證交所_5秒大盤指數資料_2023-now/{y}'
 
-# taiex5_path = os.path.join(os.path.dirname(__file__),taiex5_path)
 
-# if os.path.exists(taiex5_path):
-    # os.makedirs("data/data")
+if not os.path.exists(taiex5_path):
+    os.makedirs(taiex5_path)
 
-os.makedirs("/data/data")
-# print(taiex5_path)
-# month_list = os.listdir(opt_path)
+month_list = os.listdir(opt_path)
 
-# for m in month_list:
-#     opt_path_m = os.path.join(opt_path,m)
-#     for date in tqdm(os.listdir(opt_path_m)):
-#         day = date[-6:-4]
+for m in month_list:
+    opt_path_m = os.path.join(opt_path,m)
+    for date in tqdm(os.listdir(opt_path_m)):
+        day = date[-6:-4]
         
-#         store_path = os.path.join(taiex5_path,f'{m}_{day}.db')
+        store_path = os.path.join(taiex5_path,f'{m}_{day}.db')
 
-#         if not os.path.exists(store_path):
-#             time.sleep(random.random()*8)
-#             data = []
-#             url = f'https://www.twse.com.tw/rwd/zh/TAIEX/MI_5MINS_INDEX?date={y}{m}{day}&response=html'
+        if not os.path.exists(store_path):
+            time.sleep(random.random()*8)
+            data = []
+            url = f'https://www.twse.com.tw/rwd/zh/TAIEX/MI_5MINS_INDEX?date={y}{m}{day}&response=html'
 
-#             html = requests.get(url)
-#             soup = bs(html.text,'lxml')
+            html = requests.get(url)
+            soup = bs(html.text,'lxml')
 
-#             trs = soup.tbody.find_all('tr')
-#             for tr in trs:
-#                 tds = tr.find_all('td')[:2]
-#                 data.append([i.getText().replace(',','') for i in tds])
-#             df = pd.DataFrame(data,columns=['Time','Price'])
-#             sql_func.store_data(store_path,df,table_name='taiex')
-#         break
-#     break
+            trs = soup.tbody.find_all('tr')
+            for tr in trs:
+                tds = tr.find_all('td')[:2]
+                data.append([i.getText().replace(',','') for i in tds])
+            df = pd.DataFrame(data,columns=['Time','Price'])
+            sql_func.store_data(store_path,df,table_name='taiex')
+        break
+    break
     # time.sleep(10.47393)
